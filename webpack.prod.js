98d918786e547273ba5d920e39d62c581d4a3086
Dmitry Kosykh <dmitry.kosyh@gmail.com>
2020-08-04 15:22:20 +0300

    changed mode

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,7 +4,7 @@ const CopyPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.jsx',
   output: {
     filename: 'bundle.js',
